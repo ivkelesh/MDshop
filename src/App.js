b647ce109b4ffe24d09a3515ac418a9609bdda33
Ivan Kelesh <ivan.cheles@energymarketprice.com>
2022-05-13 17:43:36 +0300

    adding bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
-import Header from './components/Header'
-import Section from './components/Section'
-import {DataProvider} from './components/Context'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+import Section from './components/Section';
+import { DataProvider } from './components/Context';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-class App extends React.Component{
-  render(){
-    return(
+class App extends React.Component {
+  render() {
+    return (
       <DataProvider>
         <div className="app">
           <Router>
             <Header />
             <Section />
+            <Footer />
           </Router>
         </div>
       </DataProvider>
